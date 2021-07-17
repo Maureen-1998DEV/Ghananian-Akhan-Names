aeec86f8d6b1fb501a3dec829afad9c776200370
Maureen-1998DEV <maureen.ougo@student.moringaschool.com>
2021-07-17 19:54:46 +0300

    modified the form.js file

--- form.js
+++ form.js
@@ -1,30 +1,32 @@
 
 
-var dateOfBirth = Number(document.querySelector("#Date").value);
-var monthOfBirth = Number(document.querySelector("#month").value);
-var yearOfMonth = Number(document.querySelector("#year").value);
-var inputButton = document.querySelector("#Akhan");
-inputButton.addEventListener("click",function(){
+let dateOfBirth;
+let monthOfBirth;
+let yearOfMonth;
+
+//submitbutton//
+const inputButton = document.querySelector("#submit");
+
+inputButton.addEventListener("click", function(){
     var dateOfBirth = Number(document.querySelector("#Date").value);
     var monthOfBirth = Number(document.querySelector("#month").value);
     var yearOfMonth = Number(document.querySelector("#year").value);
-    var inputButton = document.querySelector("#submit");
-
+    var inputDate =[dateOfBirth,monthOfBirth,yearOfMonth]
     if(dateOfBirth===0){
-        alert("Enter Date of Birth");
-    }else if(day <= 0 || day>31) {
-        alert("invalid Date of Birth");
+        alert("Enter Date of Birth😥");
+    } if(day <= 0 || day>31) {
+        alert("invalid Date of Birth😞");
     }
     if(monthOfBirth===0){
-        alert("Enter Month of Birth");
+        alert("Enter Month of Birth🙃");
     }else if(month <=0 || month >12){
-        alert("invalid month of Birth")
+        alert("invalid month of Birth😞")
     }
-    var genderMale = document.querySelector("#genderMale");
-var genderFemale = document.querySelector("#genderFemle");
+    //Akhan Names in Arrays
+
     var maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
+   if(document.querySelector("#maleGender").checked && (inputDate !=""))
 
 })
 
